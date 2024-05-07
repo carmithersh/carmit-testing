a0a75f401370ff629a0cad0a1f1d8220d2a07553
carmit hershman <78722358+carmithersh@users.noreply.github.com>
2024-05-07 09:29:45 +0300

    Update pythonProj.py
    commit with a security vulnerability

--- pythonExample/pythonProj.py
+++ pythonExample/pythonProj.py
@@ -6,5 +6,14 @@ def printme( str ):
     print (str)
     return;
 
+from project import db, app
+from Cryptodome.Cipher import ARC4
+import hashlib
+
+def arc4_encrypt_password(key, password):
+    cipher = ARC4.new(key.encode('utf-8'))
+    encrypted_password = cipher.encrypt(password.encode('utf-8'))
+    return hashlib.md5(encrypted_password).hexdigest()
+
 # Now you can call printme function
 printme("Hello from JFROG");
