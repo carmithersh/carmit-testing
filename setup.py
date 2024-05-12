bed8e97dfa39b07398224c7821b4247eaffd60de
carmit hershman <78722358+carmithersh@users.noreply.github.com>
2024-05-12 14:52:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='jfrog-python-example',
     version='1.0',
-    description='Project example for building Python project with JFrog products',
+    description='Project example for building Python project with JFrog products.',
     author='JFrog',
     author_email='jfrog@jfrog.com',
     url='https://github.com/carmithersh/carmit-testing',
