54408c668d3a2a51698d22c4925e70ffd9c13ff0
carmith <carmith@jfrog.com>
2024-12-01 11:57:36 +0200

    prep for snow 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='jfrog-python-example',
-    version='1.2',
+    version='1.3',
     description='Project example for building Python project with JFrog products.',
     author='JFrog',
     author_email='jfrog@jfrog.com',
