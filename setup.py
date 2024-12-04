611e4413c9ade50e0e3d0242380cfce3e7ede507
carmith <carmith@jfrog.com>
2024-12-04 14:25:20 +0200

    new build - rb create and promotion request

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='jfrog-python-example',
-    version='1.4',
+    version='1.5',
     description='Project example for building Python project with JFrog products.',
     author='JFrog',
     author_email='jfrog@jfrog.com',
