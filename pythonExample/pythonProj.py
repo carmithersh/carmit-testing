63face8d69affcdb1afe454ebcf6d8e1272a64db
Carmit Hershman <carmith@jfrog.com>
2026-02-25 17:36:31 +0200

    CTOA-1013 - Add additional print statement in pythonProj.py for enhanced output clarity

--- pythonExample/pythonProj.py
+++ pythonExample/pythonProj.py
@@ -20,3 +20,4 @@ def arc4_encrypt_password(key, password):
 # Now you can call printme function
 printme("Hello from JFROG!!");
 printme("I am part of test skill 4 jira!!");
+printme("I am part of test skill 4 commit 2");
