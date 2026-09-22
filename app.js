0de74599ee898477a9bc0740f312c22931cbb7ae
Carmit Hershman <carmith@jfrog.com>
2026-09-22 16:55:41 +0300

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Frogs! \n ');
+  res.end('Hello Frogs, here I am! \n ');
 });
 
 server.listen(port, hostname, () => {
